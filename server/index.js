f3cfc7cc0d71dc65960a6ec1cea7ce8314de1c9e
basic <milanbasic67@gmail.com>
2026-05-27 00:59:16 +0300

    fix railway bindingm

--- server/index.js
+++ server/index.js
@@ -494,17 +494,21 @@ app.post(
 // START SERVER
 const PORT =
   process.env.PORT || 5000;
+
 app.listen(
 
   PORT,
-"0.0.0.0"
+
+  "0.0.0.0",
+
   () => {
 
     console.log(
 
-      `Server running on port ${PORT}`
+      `Server running on ${PORT}`
 
     );
 
   }
+
 );
