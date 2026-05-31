0b85badcd394fd2e3e46987ed30f54519ebd23f8
basic <milanbasic67@gmail.com>
2026-05-31 19:36:15 +0300

    workikbasic3168

--- server/index.js
+++ server/index.js
@@ -71,7 +71,7 @@ app.post("/render-duet", upload.single("reaction"), async (req, res) => {
     console.log("Pokrećem FFmpeg...");
     
     ffmpeg()
-        .input(originalUrl)
+        .input(localMp4Path)
         .input(reactionFile.path)
         .duration(finalDuration + 0.5) // Mala margina
         .complexFilter([
